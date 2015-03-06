0d1a697979a11f4ceb39a617cc48341873d8e986
manolobiene <manel-360@outlook.com>
2015-03-06 23:42:40 +0100

    New Commit
    :D

---
+++ Assets/movie.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movie : MonoBehaviour {
+	public MovieTexture mov;
+
+	void Start () {
+		GetComponent<Renderer> ().material.mainTexture = mov;
+		GetComponent<AudioSource> ().clip = mov.audioClip;
+		mov.Play ();
+		GetComponent<AudioSource> ().Play ();
+	}
+}
